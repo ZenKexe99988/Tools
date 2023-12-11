8b8620edcf768b6d98ae78c9b09fa1cea1703d33
ZenKexe99988 <152761637+ZenKexe99988@users.noreply.github.com>
2023-12-11 08:22:16 +0700

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,50 @@
+import os
+
+print("""
+t.me/BlackHatStoreReal
+""")
+
+print("""[1] pip\n[2] pip3\nYang mana anda mau pilih?""")
+rn = input(">>>")
+if rn == '1':
+    os.system("pkg update -y")
+    os.system("pkg upgrade -y")
+    os.system("pip install speedtest-cli")
+    os.system("pip install PySocks")
+    os.system("pip install cfscrape")
+    os.system("pip install datetime")
+    os.system("pip install ssl")
+    os.system("pip install threading")
+    os.system("pip install imcplib")
+    os.system("pip install scapy")
+elif rn == '2':
+    os.system("pkg update -y")
+    os.system("pkg upgrade -y")
+    os.system("pip3 install speedtest-cli")
+    os.system("pip3 install PySocks")
+    os.system("pip3 install cfscrape")
+    os.system("pip3 install datetime")
+    os.system("pip3 install ssl")
+    os.system("pip3 install threading")
+    os.system("pip3 install imcplib")
+    os.system("pip3 install scapy")
+else:
+    print("?")
+
+os.system("pkg install nodejs -y")
+os.system("pkg install npm -y")
+os.system("npm i events")
+os.system("npm i fs")
+os.system("npm i url")
+os.system("npm i net")
+os.system("npm i cloudscraper")
+os.system("npm i request")
+os.system("npm i randomstring")
+os.system("npm i cluster")
+os.system("npm i cloudflare-bypasser")
+os.system("npm i hcaptcha-solver")
+os.system("ulimit -n 999999")
+os.system("chmod +x *")
+os.system("pkg update -y")
+os.system("pkg upgrade -y")
+os.system("python3 sage.py")
